e730d317f5ab88a0833e1c9dec33ddebc24e3179
pof-cpu <63476556+pof-cpu@users.noreply.github.com>
2021-07-12 16:43:50 -1000

    20210712

---
+++ Accueil/accueil.js
@@ -0,0 +1,28 @@
+//  Pour obtenir la date en lettre en Fr
+// ********************************************
+let today = new Date();
+ let datelocale = today.toLocaleDateString('fr-FR');
+ let datelocalelongue = today.toLocaleDateString('fr-FR',{
+     weekday:'long',
+     year : 'numeric',
+     month:'long',
+     day: 'numeric'
+ });
+document.getElementById("date").innerHTML=datelocalelongue;
+ 
+function GetFileInfo () {
+    let message = "";
+    let date = new Date(document.lastModified);
+    date = date.toLocaleDateString('fr-FR',{
+        weekday:'long',
+        year : 'numeric',
+        month:'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute : 'numeric'
+    });
+    message = "Document mis à jour le " + date;
+    document.getElementById("last-modified").innerHTML = message+'.';
+}
+console.log(GetFileInfo ());
+console.log(typeof(today));
